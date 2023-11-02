9b5a22992f4a50446aa800aea9073319a2b0a4ca
Camper_Samu <32773961+CamperSamu@users.noreply.github.com>
2023-11-02 19:44:23 +0100

    Fix path

--- derp_salami.mk
+++ derp_salami.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/salami/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_salami
 PRODUCT_DEVICE := salami
